3552b4545db6db171b10217c3e3c78c8820364bd
Bhavye Mathur <bhavyemathur@gmail.com>
2024-07-01 01:14:25 +0530

    Added a get_device() function

---
+++ aivolved/device.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def get_device() -> str:
+    if torch.cuda.is_available():
+        return "cuda"
+    if torch.backends.mps.is_available():
+        return "mps"
+    return "cpu"
+
+
+__all__ = ["get_device"]
