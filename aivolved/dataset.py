6959e79b736567bec5daee0589cbe0025ffaa54b
Bhavye Mathur <bhavyemathur@gmail.com>
2024-07-01 00:58:07 +0530

    Added function to get dataset image mean & stdev

--- aivolved/dataset.py
+++ aivolved/dataset.py
@@ -1,3 +1,5 @@
+import torchvision
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 from .path import get_image_sources, directory_from_files
@@ -13,4 +15,11 @@ def split_dataset(src: str, dst: str, label: str) -> None:
     directory_from_files(f"{dst}/val/{label}", val)
 
 
-__all__ = ["split_dataset"]
+def get_img_dataset_normalisation(dataset):
+    data = np.array([np.array(img) for img, _ in dataset])
+    mean = data.mean(axis=(0, 1, 2)) / 255
+    stdev = data.std(axis=(0, 1, 2)) / 255
+    return mean, stdev
+
+
+__all__ = ["split_dataset", "get_img_dataset_normalisation"]
